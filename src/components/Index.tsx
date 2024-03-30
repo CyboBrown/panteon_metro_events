1d13a4e5f4b1318f1b6eb7a2bd7324165e9bb8ea
Jethan <jethanarita@gmail.com>
2024-03-30 17:01:26 +0800

    theming

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -4,34 +4,47 @@ import Link from "@mui/material/Link";
 import { Stack } from "@mui/material";
 import Button from '@mui/material/Button';
 import './Index.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
 
 export default function Index() {
   
   const linkSX = {
-    color: 'black',
+    color: 'white',
   }
 
-  const buttonSX = {
-    background: '#F5F5DC'
-  }
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#CAB535'
+      },
+      secondary: {
+        main: '#35CAB5'
+      }
+    }
+  })
 
   return (
     <>
-      <div className="title">
-        <h1>Metro Events</h1>
-      </div>
-      <Stack 
-        direction="row"
-        spacing={2}
-        justifyContent='center'
-        sx={{paddingTop: 5}}>
-        <Button variant="contained" sx={buttonSX}>
-          <Link href="/signin" underline='none' sx={linkSX}>Sign in</Link>
-        </Button>
-        <Button variant="contained" sx={buttonSX}>
-          <Link href="/signup" underline='none' sx={linkSX}>Sign up</Link>
-        </Button>
-      </Stack>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <div className="title">
+            <h1>Metro Events</h1>
+          </div>
+          <Stack 
+            direction="row"
+            spacing={2}
+            justifyContent='center'
+            sx={{paddingTop: 5}}>
+            <Button variant="contained">
+              <Link href="/signin" underline='none' sx={linkSX}>Sign in</Link>
+            </Button>
+            <Button variant="contained">
+              <Link href="/signup" underline='none' sx={linkSX}>Sign up</Link>
+            </Button>
+          </Stack>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
