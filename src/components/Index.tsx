92377ceb2854cf278147e814c64bbcf800dfd843
Jethan <jethanarita@gmail.com>
2024-03-30 16:11:48 +0800

    cleaned index

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import Link from "@mui/material/Link";
+import { Stack } from "@mui/material";
+import Button from '@mui/material/Button';
+import './Index.css';
 
 export default function Index() {
-  const [count, setCount] = useState(0);
+  
+  const linkSX = {
+    color: 'black',
+  }
+
+  const buttonSX = {
+    background: '#F5F5DC'
+  }
 
   return (
     <>
-      <h1>Metro Events - Home</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div>
-          <Link to="/signin">Go to Sign In</Link>
-        </div>
-        <div>
-          <Link to="/signup">Go to Sign Up</Link>
-        </div>
-        <div>
-          <Link to="/home">Go to Home</Link>
-        </div>
+      <div className="title">
+        <h1>Metro Events</h1>
       </div>
+      <Stack 
+        direction="row"
+        spacing={2}
+        justifyContent='center'
+        sx={{paddingTop: 5}}>
+        <Button variant="contained" sx={buttonSX}>
+          <Link href="/signin" underline='none' sx={linkSX}>Sign in</Link>
+        </Button>
+        <Button variant="contained" sx={buttonSX}>
+          <Link href="/signup" underline='none' sx={linkSX}>Sign up</Link>
+        </Button>
+      </Stack>
     </>
   );
 }
